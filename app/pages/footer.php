129d34502012504fa61aa3c8c054a78b27e84cf2
meemimee <kerokerotara@gmail.com>
2025-03-26 13:25:23 +0900

    footer chotto chousei

--- app/pages/footer.php
+++ app/pages/footer.php
@@ -1,8 +1,6 @@
 </div> <!-- .content の終了タグ -->
     <footer class="footer">
-        <div class="container">
-            <p>&copy; <?php echo date('Y'); ?> Washi Ga Tukutta! </p>
-        </div>
+            <p>&copy; <?php echo date('Y'); ?>  Washi Ga Tukutta! </p>
     </footer>
 </body>
 </html>
